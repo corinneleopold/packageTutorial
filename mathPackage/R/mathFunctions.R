6cdac803012ffe27bf0b0e3811fb25c7e86f6065
Corinne Leopold <Corinne.Leopold@ibm.com>
2019-05-01 17:56:16 -0500

    added package

---
+++ mathPackage/R/mathFunctions.R
@@ -0,0 +1,27 @@
+#' My Sum
+#'
+#' Returns the sum of 2 numbers
+#' @param x A numeric value to sum
+#' @param y A numeric value to sum
+#' @keywords add
+#' @export
+#' @examples
+#' my_sum(4, 5)
+
+my_sum <- function(x, y) {
+  return(x + y)
+}
+
+#' My Multiplication
+#'
+#' Returns the product of 2 numbers
+#' @param x A numeric value to multiply
+#' @param y A numeric value to multiply
+#' @keywords multiply product
+#' @export
+#' @examples
+#' my_mult(4, 5)
+
+my_mult <- function(x, y) {
+  return(x * y)
+}
